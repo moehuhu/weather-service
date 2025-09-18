2b0cfc4642bc502e037fd608fcd8ae850b40f85f
moehuhu <994458697@qq.com>
2025-09-18 22:15:17 +0800

    获取地址，查询表

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+    console.log(env)
+    const position = request.url
+    console.log(position)
+    const stmt = env.DB.prepare("SELECT * FROM position_weather");
     const { results } = await stmt.all();
 
     return new Response(JSON.stringify(results));
