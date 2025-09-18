35cfd88fcdcf682bf951f693b1fce8466cc2c0e5
moehuhu <994458697@qq.com>
2025-09-18 23:20:42 +0800

    测试缓存

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-
+import dayjs from 'dayjs'
 export default {
   async fetch(request, env) {
     console.log(env)
     const position = request.url.split('/').pop()
     const stmt = env.DB.prepare("SELECT * FROM position_weather WHERE position = ?");
     const { results } = await stmt.bind(position).all();
-    if (results.length === 0) {
-      const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
-      const weatherResponse = await fetch(weatherURL)
-      const weatherData = await weatherResponse.text()
-      env.DB.prepare("INSERT INTO position_weather (position, weather, date) VALUES (?, ?, ?)")
-        .bind(position, JSON.stringify(weatherData), new Date().toISOString())
-        .run()
+    if (results.length !== 0) {
+      const nowDate = dayjs()
+      const weatherData = results[0].weather
+      const oldDate = dayjs(results[0].date)
+      if (nowDate.diff(oldDate, 'hour') > 2) {
+        env.DB.prepare("UPDATE position_weather SET weather = ?, date = ? WHERE position = ?")
+          .bind(JSON.stringify(weatherData), new Date().toISOString(), position)
+          .run()
+      }
       return new Response(JSON.stringify(weatherData));
     }
-
+    const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
+    const weatherResponse = await fetch(weatherURL)
+    const weatherData = await weatherResponse.text()
+    env.DB.prepare("INSERT INTO position_weather (position, weather, date) VALUES (?, ?, ?)")
+      .bind(position, JSON.stringify(weatherData), dayjs().toISOString)
+      .run()
     return new Response(JSON.stringify(results[0]));
   },
 } satisfies ExportedHandler<Env>;
