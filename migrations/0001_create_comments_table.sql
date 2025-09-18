190b1267f8388b5e34bf5d8f1b32e5d9d517ac02
moehuhu <994458697@qq.com>
2025-09-18 22:06:59 +0800

    Initial Commit

--- migrations/0001_create_comments_table.sql
+++ migrations/0001_create_comments_table.sql
@@ -1,14 +1,7 @@
 -- Migration number: 0001 	 2024-12-27T22:04:18.794Z
-CREATE TABLE IF NOT EXISTS comments (
+CREATE TABLE IF NOT EXISTS position_weather (
     id INTEGER PRIMARY KEY NOT NULL,
-    author TEXT NOT NULL,
-    content TEXT NOT NULL
+    position TEXT NOT NULL,
+    weather TEXT NOT NULL,
+    date TEXT NOT NULL
 );
-
--- Insert some sample data into our comments table.
-INSERT INTO comments (author, content)
-VALUES
-    ('Kristian', 'Congrats!'),
-    ('Serena', 'Great job!'),
-    ('Max', 'Keep up the good work!')
-;
