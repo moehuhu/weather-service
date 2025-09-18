ce8476705a746565757a16a8140133c370885a01
moehuhu <994458697@qq.com>
2025-09-18 23:07:12 +0800

    测试

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ export default {
   async fetch(request, env) {
     console.log(env)
     const position = request.url.split('/').pop()
-    const stmt = env.DB.prepare("SELECT * FROM position_weather");
-    const { results } = await stmt.all();
-    const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
-    const weatherResponse = await fetch(weatherURL)
-    const weatherData = await weatherResponse.text()
-    return new Response(JSON.stringify({ weatherData }));
+    const stmt = env.DB.prepare("SELECT * FROM position_weather WHERE position = ?");
+    const { results } = await stmt.bind(position).all();
+    if (results.length === 0) {
+      const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
+      const weatherResponse = await fetch(weatherURL)
+      const weatherData = await weatherResponse.text()
+      env.DB.prepare("INSERT INTO position_weather (position, weather, date) VALUES (?, ?, ?)")
+        .bind(position, JSON.stringify(weatherData), new Date().toISOString())
+        .run()
+      return new Response(JSON.stringify(weatherData));
+    }
+
+    return new Response(JSON.stringify(results[0]));
   },
 } satisfies ExportedHandler<Env>;
