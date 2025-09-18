9731fb7b627ddcae97fb0b09ce1bd40fa27c3498
moehuhu <994458697@qq.com>
2025-09-18 23:58:45 +0800

    类型

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 export default {
   async fetch(request, env) {
-    console.log(env)
     const position = request.url.split('/').pop()
     const stmt = env.DB.prepare("SELECT * FROM position_weather WHERE position = ?");
     const { results } = await stmt.bind(position).all();
+    const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
+
     if (results.length !== 0) {
-      const weatherData = results[0].weather
+      const oldWeather = results[0].weather
       const oldDate = new Date(results[0].date)
       if (Date.now() - oldDate.getTime() > 2 * 60 * 60 * 1000) {
+        const weatherResponse = await fetch(weatherURL)
+        const weatherData = await weatherResponse.text()
         await env.DB.prepare("UPDATE position_weather SET weather = ?, date = ? WHERE position = ?")
-          .bind(JSON.stringify(weatherData), new Date().toISOString(), position)
+          .bind(weatherData, new Date().toISOString(), position)
           .run()
+        return new Response(weatherData);
       }
-      return new Response(JSON.parse(weatherData));
+      return new Response(oldWeather);
     }
-    const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
     const weatherResponse = await fetch(weatherURL)
     const weatherData = await weatherResponse.text()
     await env.DB.prepare("INSERT INTO position_weather (position, weather, date) VALUES (?, ?, ?)")
       .bind(position, weatherData, new Date().toISOString())
       .run()
-    return new Response(JSON.parse(weatherData));
+    return new Response(weatherData);
   },
 } satisfies ExportedHandler<Env>;
