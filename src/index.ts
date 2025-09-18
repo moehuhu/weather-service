de185698f146b71c4f8e6f8d7b887a56dc179a9e
moehuhu <994458697@qq.com>
2025-09-18 22:36:34 +0800

    查询天气

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
 export default {
   async fetch(request, env) {
     console.log(env)
-    const position = request.url
-    console.log(position)
+    const position = request.url.split('/').pop()
     const stmt = env.DB.prepare("SELECT * FROM position_weather");
     const { results } = await stmt.all();
-
-    return new Response(JSON.stringify({ position, env }));
+    const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}`
+    const weatherResponse = await fetch(weatherURL)
+    const weatherData = await weatherResponse.json()
+    console.log(weatherData)
+    return new Response(JSON.stringify({ position, weatherData }));
   },
 } satisfies ExportedHandler<Env>;
