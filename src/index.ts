7657ee1b6809a15f335c86dbf2f4bf9e5a0636f8
moehuhu <994458697@qq.com>
2025-09-18 23:38:45 +0800

    加await

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export default {
       const weatherData = results[0].weather
       const oldDate = new Date(results[0].date)
       if (Date.now() - oldDate.getTime() > 2 * 60 * 60 * 1000) {
-        env.DB.prepare("UPDATE position_weather SET weather = ?, date = ? WHERE position = ?")
+        await env.DB.prepare("UPDATE position_weather SET weather = ?, date = ? WHERE position = ?")
           .bind(JSON.stringify(weatherData), new Date().toISOString(), position)
           .run()
       }
@@ -17,7 +17,7 @@ export default {
     const weatherURL = `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}/${position}/daily?dailysteps=7`
     const weatherResponse = await fetch(weatherURL)
     const weatherData = await weatherResponse.text()
-    env.DB.prepare("INSERT INTO position_weather (position, weather, date) VALUES (?, ?, ?)")
+    await env.DB.prepare("INSERT INTO position_weather (position, weather, date) VALUES (?, ?, ?)")
       .bind(position, JSON.stringify(weatherData), new Date().toISOString())
       .run()
     return new Response('未缓存天气');
