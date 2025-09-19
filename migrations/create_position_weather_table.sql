58a55fa1446e7a2a88514c4fa93f6788ce455a06
moehuhu <994458697@qq.com>
2025-09-19 21:29:04 +0800

    修改表结构

--- migrations/create_position_weather_table.sql
+++ migrations/create_position_weather_table.sql
@@ -1,7 +1,9 @@
 -- Migration number: 0001 	 2024-12-27T22:04:18.794Z
 CREATE TABLE IF NOT EXISTS position_weather (
     id INTEGER PRIMARY KEY NOT NULL,
-    position TEXT NOT NULL,
+    longitude REAL NOT NULL,
+    latitude REAL NOT NULL,
     weather TEXT NOT NULL,
-    date TEXT NOT NULL
+    updated_at TEXT NOT NULL,
+    success BOOLEAN NOT NULL
 );
