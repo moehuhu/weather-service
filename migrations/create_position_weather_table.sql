c8b693723617212574725a5949d5e6e96668feaf
moehuhu <994458697@qq.com>
2025-09-20 13:57:07 +0800

    Revert "删除sql文件"
    This reverts commit 94e99fe38153139b0213f3af4d276df0ddac1aa7.

---
+++ migrations/create_position_weather_table.sql
@@ -0,0 +1,9 @@
+-- Migration number: 0001 	 2024-12-27T22:04:18.794Z
+CREATE TABLE IF NOT EXISTS position_weather (
+    id INTEGER PRIMARY KEY NOT NULL,
+    longitude REAL NOT NULL,
+    latitude REAL NOT NULL,
+    weather TEXT NOT NULL,
+    updated_at INTEGER NOT NULL,
+    success INTEGER NOT NULL
+);
