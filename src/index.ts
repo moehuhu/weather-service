c35090647eee29497e7472371e532fae695e5688
moehuhu <994458697@qq.com>
2025-09-20 12:37:51 +0800

    重构缓存

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,30 @@ export default {
       return weatherData
     }
 
+    const db = drizzle(env.DB)
+    const getOldWeatherRecord = async (longitude: number, latitude: number) => {
+      const weather = await db
+        .select()
+        .from(weatherTable)
+        .where(and(eq(weatherTable.longitude, longitude), eq(weatherTable.latitude, latitude)))
+        .limit(1)
+      return weather[0]
+    }
+    const addNewWeather = async (longitude: number, latitude: number, weatherData: string) => {
+      await db.insert(weatherTable).values({
+        longitude: longitude,
+        latitude: latitude,
+        weather: weatherData,
+        updated_at: Date.now(),
+      })
+    }
+    const updateWeather = async (longitude: number, latitude: number, weatherData: string) => {
+      await db.update(weatherTable).set({
+        weather: weatherData,
+        updated_at: Date.now(),
+      }).where(and(eq(weatherTable.longitude, longitude), eq(weatherTable.latitude, latitude)))
+    }
+
     const requestParams = request.url.split('?')?.[1]
     const params: Record<string, string> = {}
     requestParams?.split('&').map(item => item.split('=')).forEach(item => params[item[0]] = item[1])
@@ -46,28 +70,27 @@ export default {
     if (longitude === undefined || latitude === undefined) {
       return new Response(JSON.stringify({ error: { message: 'Invalid request' } }), { status: 400, headers: { 'Content-Type': 'application/json' } })
     }
+    const position = `${longitude},${latitude}`
 
-    const db = drizzle(env.DB)
-    const weather = await db
-      .select()
-      .from(weatherTable)
-      .where(and(eq(weatherTable.longitude, Number(longitude)), eq(weatherTable.latitude, Number(latitude))))
-      .limit(1)
-    if (weather.length > 0) {
-      return new Response(weather[0].weather, { headers: { 'Content-Type': 'application/json' } })
+    const weatherRecord = await getOldWeatherRecord(Number(longitude), Number(latitude))
+    if (weatherRecord) {
+      const isExpired = weatherRecord.updated_at + 4 * 60 * 60 * 1000 < Date.now()
+      if (isExpired) {
+        const [error, newWeatherData] = await to(fetchWeather(position))
+        if (error) {
+          return new Response(JSON.stringify({ error: { message: error.message } }), { status: 500, headers: { 'Content-Type': 'application/json' } })
+        }
+        await updateWeather(Number(longitude), Number(latitude), newWeatherData)
+        return new Response(newWeatherData, { headers: { 'Content-Type': 'application/json' } })
+      }
+      return new Response(weatherRecord.weather, { headers: { 'Content-Type': 'application/json' } })
     }
 
-    const position = `${longitude},${latitude}`
     const [error, weatherData] = await to(fetchWeather(position))
     if (error) {
       return new Response(JSON.stringify({ error: { message: error.message } }), { status: 500, headers: { 'Content-Type': 'application/json' } })
     }
-    await db.insert(weatherTable).values({
-      longitude: Number(longitude),
-      latitude: Number(latitude),
-      weather: weatherData,
-      updated_at: Date.now(),
-    })
+    await addNewWeather(Number(longitude), Number(latitude), weatherData)
     return new Response(weatherData, { headers: { 'Content-Type': 'application/json' } });
   },
 } satisfies ExportedHandler<Env>;
