347cf8974c9fc78914205b4549915c2420c80059
moehuhu <994458697@qq.com>
2025-09-20 16:32:05 +0800

    修改变量名

--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,7 @@ export default {
 
     const weatherRecord = await getOldWeatherRecord(Number(longitude), Number(latitude))
     if (weatherRecord) {
-      const isExpired = weatherRecord.updated_at + Number(env.INTERVAL) * 60 * 1000 < Date.now() || new Date(weatherRecord.updated_at).getDate() !== new Date().getDate()
+      const isExpired = weatherRecord.updated_at + Number(env.INTERVAL_MINUTES) * 60 * 1000 < Date.now() || new Date(weatherRecord.updated_at).getDate() !== new Date().getDate()
       if (isExpired) {
         const [error, newWeatherData] = await to(fetchWeather(position))
         if (error) {
